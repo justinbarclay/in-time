c984f7320b8f91a01aff72841a8501d5dd01329c
Justin <justincbarclay@gmail.com>
2015-03-09 00:37:08 -0600

    Create a script that will generate a testUser table

---
+++ scripts/createUserDB.js
@@ -0,0 +1,13 @@
+//this is an iojs script to recreate the user database in postgresvar conString = "pg://postgres:aldorak@localhost:5432/employees";
+var client = new pg.Client(conString);
+client.connect();
+
+client.query("DROP TABLE IF EXISTS Users");
+// Creat table and insert 2 records into it
+
+try{
+client.query("CREATE TABLE IF NOT EXISTS TestUsers ( id serial PRIMARY KEY, username varchar(32) UNIQUE,  password varchar(32) , email varchar(32) UNIQUE)");
+}
+catch (error) {
+  console.log(error);
+}
