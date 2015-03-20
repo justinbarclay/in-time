e1f3f7a21978aee462c1394e2e247e9793c5da7f
Justin <justincbarclay@gmail.com>
2015-03-19 23:47:39 -0600

    Working server then can take a post request

--- server.js
+++ server.js
@@ -11,13 +11,29 @@ var server = http.createServer(function(req, res){
   if(path === '/') {                    //Pseudo code, check the path request and make sure it's base path
       console.log(path);
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(fs.readFileSync('app/public/test.html'));
+      res.write(fs.readFileSync('app/public/index.html'));
 
       res.end();
       console.log('Successful test');
-  } else {
+  } else if (path === '/login') {
+    console.log("Succesful post!");
+    var data = '';
+    req.on("data", function(chunk){
+      data += chunk;
+    });
+    req.on("end", function(){
+      user = querystring.parse(data);
+      console.log(user.username);
+      res.write("Success!");
+      res.end();
+    });
+
+
+  } else  {
       res.writeHead(404);
       console.log("fail");
+      console.log(req.headers);
+      console.log(req.body);
       res.end();
   }
 });
