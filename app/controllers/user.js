6849971b1f3150b7491e3ec3002107398a94e2e3
Justin Barclay <justincbarclay@gmail.com>
2015-05-29 18:19:27 -0600

    Beautify user controller and remove all instances of client.end(); from the controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,8 +1,8 @@
 config = require("../../config.json");
 /** User Controller
-* This will handle creating a user, removing a user, and authenticating a loging
-*
-*/
+ * This will handle creating a user, removing a user, and authenticating a loging
+ *
+ */
 var pg = require('pg');
 var conString = config.postgres;
 
@@ -10,127 +10,150 @@ var conString = config.postgres;
 
 var bcrypt = require('bcrypt');
 
-function hashPassword(userPassword, callback){
-//this function will hash the password, useful for any interactions with our
-//user system
-bcrypt.genSalt(10, function(err, salt) {
-bcrypt.hash(userPassword, salt, function(err, hash){
-  callback(err, hash);
-});
-});
-}
-
-function signUpUser(userName, userPassword,  userEmail, callback){
-//this function creates a username and hashes
-//a password then stor it in the database
-pg.connect(conString, function(err, client, done) {
-if(err) {
-  return console.error('error fetching client from pool in user controller', err);
-}
-queryString =  "SELECT * FROM userlogin WHERE username="  + "'"+userName+ "'";
-client.query(queryString, function(err, result) {
-  //call `done()` to release the client back to the pool
-
-  if(err) {
-    done();
-    return console.error('error running select query', err);
-  } else if (typeof result.rows[0] === 'undefined'){
-      console.log(userPassword);
-    hashPassword(userPassword, function(err, hash){
-
-      if (err){
-        done();
-        return console.error("error hashing password", err);
-      } else {
-        client.query("INSERT INTO UserLogin(username, password, email) values($1, $2, $3)", [userName, hash, userEmail], function(err, result){
-          done();
-          if (err) {
-            console.error('error running insert query', err);
-            callback(err, false);
-          } else {
-              console.log(result);
-              console.log("User created successfully");
-            callback(err, true, "User created successfully" );
-          }
-          client.end();
+function hashPassword(userPassword, callback) {
+    //this function will hash the password, useful for any interactions with our
+    //user system
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(userPassword, salt, function(err, hash) {
+            callback(err, hash);
         });
-      }
+    });
+}
+
+function signUpUser(userName, userPassword, userEmail, callback) {
+    //this function creates a username and hashes
+    //a password then stor it in the database
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            return console.error(
+                'error fetching client from pool in user controller',
+                err);
+        }
+        queryString = "SELECT * FROM userlogin WHERE username=" + "'" +
+            userName + "'";
+        client.query(queryString, function(err, result) {
+            //call `done()` to release the client back to the pool
+
+            if (err) {
+                done();
+                return console.error(
+                    'error running select query', err);
+            } else if (typeof result.rows[0] === 'undefined') {
+                console.log(userPassword);
+                hashPassword(userPassword, function(err, hash) {
+
+                    if (err) {
+                        done();
+                        return console.error(
+                            "error hashing password",
+                            err);
+                    } else {
+                        client.query(
+                            "INSERT INTO UserLogin(username, password, email) values($1, $2, $3)", [
+                                userName, hash,
+                                userEmail
+                            ],
+                            function(err, result) {
+                                done();
+                                if (err) {
+                                    console.error(
+                                        'error running insert query',
+                                        err);
+                                    callback(err,
+                                        false);
+                                } else {
+                                    console.log(
+                                        result);
+                                    console.log(
+                                        "User created successfully"
+                                    );
+                                    callback(err,
+                                        true,
+                                        "User created successfully"
+                                    );
+                                }
+
+                            });
+                    }
+                });
+
+            } else {
+                console.log("Username all ready taken");
+                done();
+                callback(err, false,
+                    "Username or e-mail is unavailable");
+            }
+        });
     });
 
-  } else {
-    console.log("Username all ready taken");
-    done();
-    callback(err, false, "Username or e-mail is unavailable");
-    client.end();
-  }
-
-
-});
-});
-
 
 }
 
-function deleteUser(userName){
-//deletes user from database
-pg.connect(conString, function(err, client, done) {
-if(err) {
-  done();
-  client.end();
-  return console.error('error fetching client from pool', err);
-}
-client.query("DELETE FROM UserLogin WHERE username =" + "'" + userName + "'", function(err, res){
-  if(err){
-    console.error('error with a DELETE query',err);
-    done();
-    client.end();
-  } else {
-    console.log(res);
-  }
-  done();
-  client.end();
-});
+function deleteUser(userName) {
+    //deletes user from database
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            done();
 
-});
-}
+            return console.error('error fetching client from pool', err);
+        }
+        client.query("DELETE FROM UserLogin WHERE username =" + "'" +
+            userName + "'",
+            function(err, res) {
+                if (err) {
+                    console.error('error with a DELETE query', err);
+                    done();
 
-function authenticateUser(userName, userPassword, callback){
-//this function checks to see if the userName and userPassword match
-//anything stored in the user database
-pg.connect(conString, function(err, client, done) {
-if(err) {
-  return console.error('error fetching client from pool', err);
-}
-client.query("SELECT * FROM UserLogin WHERE username =" + "'" + userName + "'", function(err, res){
-  if(err){
-    done();
-    client.end();
-    callback(err, false);
-  } else if(typeof(res.rows[0]) === 'undefined'){
-    done();
-    client.end();
-    console.log(res);
-    callback(err, false);
-  } else {
-    bcrypt.compare(userPassword, res.rows[0].password, function(err, res){
-      if (err) {
-        // code to add token to browser to act logged in
-        // probably need to add a token to table somewhere as well
-        done();
-        client.end();
-        callback(err, res);
-      } else {
-        done();
-        client.end();
-        callback(err, res);
-      }
+                } else {
+                    console.log(res);
+                }
+                done();
+
+            });
 
     });
-    done();
-    client.end();
-  }
-});
-});
+}
+
+function authenticateUser(userName, userPassword, callback) {
+    //this function checks to see if the userName and userPassword match
+    //anything stored in the user database
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query("SELECT * FROM UserLogin WHERE username =" + "'" +
+            userName + "'",
+            function(err, res) {
+                if (err) {
+                    done();
+
+                    callback(err, false);
+                } else if (typeof(res.rows[0]) === 'undefined') {
+                    done();
+
+                    console.log(res);
+                    callback(err, false);
+                } else {
+                    bcrypt.compare(userPassword, res.rows[0].password,
+                        function(err, res) {
+                            if (err) {
+                                // code to add token to browser to act logged in
+                                // probably need to add a token to table somewhere as well
+                                done();
+
+                                callback(err, res);
+                            } else {
+                                done();
+
+                                callback(err, res);
+                            }
+
+                        });
+                    done();
+
+                }
+            });
+    });
 }
 /**
 function helper(){
@@ -144,7 +167,7 @@ pg.connect(conString, function(err, client, done){
 function databaseError(err, callback)
 {
 done();
-client.end();
+
 callback(err, false);
 }
 function generateToken (userName) {
