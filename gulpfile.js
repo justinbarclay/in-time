ed67c4a7d58ec2909c175e7e31a2ecb54318530d
Justin Barclay <justincbarclay@gmail.com>
2015-06-01 01:48:55 -0600

    Set up sone code for gulp task runners

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass-binaries');
+var gulp = require('gulp'),
+    sass = require('gulp-sass-binaries'),
+    react = require('gulp-react'),
+    concat = require('concat');
+/*
+* TASKS
+*/
 
-gulp.task('default', function() {
-  //place code for default task here
-})
-
-
-//stolen off of gulp-sass webpage
+// /////////////////////////////////////////////
+// SASS
+// /////////////////////////////////////////////
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./css'));
+});
+
+// /////////////////////////////////////////////
+// REACT
+// /////////////////////////////////////////////
+
+gulp.task('react', function() {
+    gulp.src('./app/client/**/*.jsx')
+    .pipe(react())
+    .pipe(concat())
+    .pipe(gulp.dest('./app/client/build/main.js'));
+});
+
+/*
+* WATCH
+*/
+
+
+/*
+* DEFAULT
+*/
+gulp.task('default', function() {
+  //place code for default task here
 });
