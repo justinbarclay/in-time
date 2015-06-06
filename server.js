25846fdb997477b26da009a1ff7f26242c87b83e
Justin Barclay <justincbarclay@gmail.com>
2015-06-06 02:22:48 -0600

    Changed server.js to pull in port from config file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http'),
     querystring = require('querystring'),
     router = require('./router'),
-    url = require('url');
+    url = require('url'),
+    port = require('./config.json').port;
 
-var port = 8888;
 
 var server = http.createServer(function(req, res){
   var path = url.parse(req.url, true).pathname;
