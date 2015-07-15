062b00afed03467db2f2b225b415d08003b11185
Justin Barclay <justincbarclay@gmail.com>
2015-07-15 13:23:13 -0600

    First implementation of Flux using biff

---
+++ app/client/actions/authActions.js
@@ -0,0 +1,32 @@
+var authStore = require('../stores/authStore');
+
+var Flux = require("../biff");
+
+// Set of allowed actions
+var authActions = Flux.createActions({
+    getUserInfo: function(){
+       return {
+           recipes: authStore.getUser()
+       };
+   },
+
+    signinUser: function(user){
+       return {
+          actionType: "SIGNIN_USER",
+          user: user
+      };
+    },
+    logoutUser: function(){
+       return {
+          actionType: "LOGOUT_USER"
+      };
+    },
+    changeUserInfo: function(user){
+       return {
+          actionType: "SIGNOUT_USER",
+          user: user
+      };
+    }
+});
+
+module.exports = authActions;
---
+++ app/client/stores/authStore.js
@@ -0,0 +1,47 @@
+// This store will give the user the ability to log in and out of the system. It will expect a
+// Username, email, and JWT token to start and I might add in UserID number as well
+
+// Flux implementation
+var Flux = require('../biff');
+
+// initialize the store
+// this is a simpe object, because I only want to store one authenticated user at a time
+var _user = {};
+
+var authStore = Flux.createStore({
+    getUserInfo: function(){
+       return _user;
+   },
+
+   signinUser: function(user){
+       // this will take a user object with the properties of username, email and JWT token
+       // I may eventually grow this information to first name, last name, and other user information
+       _user.username = user.username;
+       _user.email = user.email;
+       _user.token = user.token;
+   },
+logoutUser: function(){
+       _user = {};
+   },
+updateUser: function(user){
+       // functionally the same thing as signinUser, but different names to indicate different purposes.
+       _user.username = user.username;
+       _user.email = user.email;
+       _user.token = user.token;
+   },
+}, function(payload){
+    if(payload.actionType === "SIGNIN_USER") {
+        addRecipes(payload.user);
+        this.emitChange();
+    }
+    if(payload.actionType === "LOGOUT_USER") {
+        logoutUser();
+        this.emitChange();
+    }
+    if(payload.actionType === "CHANGE_USER") {
+        deleteRecipe(payload.user);
+        this.emitChange();
+    }
+});
+
+module.exports = authStore;
