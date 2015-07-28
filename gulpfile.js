eded09b43ffab9d4a9c51787bde43967dfb4daab
Justin Barclay <justincbarclay@gmail.com>
2015-07-27 22:31:54 -0600

    Add better error handling for Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var react = require('gulp-react');
-var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var watchify = require('watchify');
@@ -35,7 +34,7 @@ gulp.task('sass', function () {
 /* This does too much and should be split to a compile and a watch statement */
 gulp.task('browserify', function() {
     var bundler = browserify({
-        entries: ['./app/client/main.js'], // Only need initial file, browserify finds the deps
+        entries: './app/client/main.js', // Only need initial file, browserify finds the deps
         transform: [reactify], // We want to convert JSX to normal javascript
         debug: true, // Gives us sourcemapping
         cache: {}, packageCache: {}, fullPaths: true, // Requirement of watchify
@@ -47,13 +46,31 @@ gulp.task('browserify', function() {
     .on('update', function () { // When any files update
         var updateStart = Date.now();
         console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
+        watcher.bundle()
+        .on("error", function(err) {
+                    gutil.log(
+                        gutil.colors.red("Browserify compile error:"),
+                        err.message,
+                        "\n\t",
+                        gutil.colors.cyan("in file"),
+                        './app/client/main.js'
+                    );
+        })  // Create new bundle that uses the cache for high performance
         .pipe(source('main.js'))
     // This is where you add uglifying etc.
         .pipe(gulp.dest('./app/public/js'));
         console.log('Updated!', (Date.now() - updateStart) + 'ms');
     })
-    .bundle() // Create the initial bundle when starting the task
+    .bundle()
+    .on("error", function(err) {
+                gutil.log(
+                    gutil.colors.red("Browserify compile error:"),
+                    err.message,
+                    "\n\t",
+                    gutil.colors.cyan("in file"),
+                    './app/client/main.js'
+                );
+    }) // Create the initial bundle when starting the task
     .pipe(source('main.js'))
     .pipe(gulp.dest('./app/public/js/'));
 });
