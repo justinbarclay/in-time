7a2041a36b5307a9acdacef9c42d5af5b18eaf88
Justin Barclay <justincbarclay@gmail.com>
2015-08-04 12:05:19 -0600

    Add JWT and verifyJWT to router

--- router.js
+++ router.js
@@ -4,16 +4,19 @@ var querystring = require('querystring'),
     url = require('url'),
     jwt = require("jsonwebtoken");
 
-var config = require("../../../config.json");
+var config = require("./config.json");
 
 const secret = config.secret;
-//Util might not be used anymore
-var verifyJWT = function(token){
-    jwt.verify(token, secret, function(err, state){
-        if(err){
+//shorthand function for verifying JWT
+var verifyJWT = function(token) {
+    jwt.verify(token, secret  , function(err, state) {
+        console.log(secret);
+        if (err) {
             console.log("JWT Error");
         }
         console.log("state");
+        console.log(state);
+        return state;
     });
 };
 
@@ -22,121 +25,162 @@ var verifyJWT = function(token){
 //controllers pass back booleans or errors or messages or a new object to indicate
 //the state of the data. How a controller indicates this is currently up for debate
 
-function route(req, res){
+function route(req, res) {
     //A very basic router that will parse the pathname out of the request object
     //and then send back a response object with some information
 
-    var path = url.parse(req.url, true).pathname;
+    var path = url.parse(req.url, true)
+        .pathname;
     console.log(path);
-    if(path === '/') {
+    if (path === '/') {
         // Load the home page
         console.log("Path: " + path);
-        fs.readFile('app/public/index.html',function (err, data){
-          res.writeHead(200, {'Content-Type': 'text/html',
-          'Content-Length':data.length});
-          res.write(data);
-          res.end();
+        fs.readFile('app/public/index.html', function(err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': data.length
+            });
+            res.write(data);
+            res.end();
         });
         console.log('Successful test');
     } else if (path === '/signup') {
-      //Attempt to login
-      //This doesn't go anywhere yet, but it does test the user controller
+        //Attempt to login
+        //This doesn't go anywhere yet, but it does test the user controller
         console.log("Succesful post to /signup!");
         var signUp = '';
 
-        req.on("data", function(chunk){
+        req.on("data", function(chunk) {
             signUp += chunk;
         });
 
-        req.on("end", function(){
+        req.on("end", function() {
             currentUser = JSON.parse(signUp);
             //hard coded email for testing
             console.log(currentUser);
-            user.signUp(currentUser.username, currentUser.password, currentUser.email, function(err, bool, message){
-            if (err){
-              res.writeHead(400, {'Content-Type': 'application/json'});
-              res.write("There was an error talking to the server");
-              console.error(err);
-              res.end();
-            } else {
-              console.log("Succesful signUp of " + currentUser.username + " = " + bool);
-              res.writeHead(200, {'Content-Type': 'application/json'});
-              res.write(''+message);
-              res.end();
-            }
-            console.log(message);
-            });
+            user.signUp(currentUser.username, currentUser.password,
+                currentUser.email,
+                function(err, bool, message) {
+                    if (err) {
+                        res.writeHead(400, {
+                            'Content-Type': 'application/json'
+                        });
+                        res.write(
+                            "There was an error talking to the server"
+                        );
+                        console.error(err);
+                        res.end();
+                    } else {
+                        console.log("Succesful signUp of " +
+                            currentUser.username + " = " + bool
+                        );
+                        res.writeHead(200, {
+                            'Content-Type': 'application/json'
+                        });
+                        res.write('' + message);
+                        res.end();
+                    }
+                    console.log(message);
+                });
         });
     } else if (path === '/signin') {
-          //Attempt to login
-          //This doesn't go anywhere yet, but it does test the user controller
-            console.log("Succesful post to /signup!");
-            var signin = '';
+        //Attempt to login
+        //This doesn't go anywhere yet, but it does test the user controller
+        console.log("Succesful post to /signup!");
+        var signin = '';
 
-            req.on("data", function(chunk){
-                signin += chunk;
-            });
+        req.on("data", function(chunk) {
+            signin += chunk;
+        });
 
-            req.on("end", function(){
-                currentUser = JSON.parse(signin);
-                console.log(currentUser.username);
-                //hard coded email for testing
-                user.authenticate(currentUser.username, currentUser.password, function(err, auth){
-                    if (err){
-                      res.write(auth.message);
-                      console.error(err);
-                      res.end();
+        req.on("end", function() {
+            currentUser = JSON.parse(signin);
+            console.log(currentUser.username);
+            //hard coded email for testing
+            user.authenticate(currentUser.username, currentUser.password,
+                function(err, auth, signedJWT) {
+                    if (err) {
+                        res.write(auth.message);
+                        console.error(err);
+                        res.end();
                     } else {
-                      console.log("Succesful signUp of " + currentUser.username + " = " + auth.success);
-                      console.log("Message:" + JSON.stringify(auth.message) + "\n" + "message length: " + auth.length);
-                      /* This header needs to reworked so that it reports the length properly, otherwise AJAX acts weird
-                      res.writeHead(200, {'Content-Type': 'application/json','Content-Length':message.length}); */
-                      res.writeHead(200, {'Content-Type': 'application/json'});
-                      //res.setHeader('x-access-token', auth.JWT);
-                      res.write(JSON.stringify(auth));
-                      res.end();
-                      console.log('response sent');
-                      console.log(req.headers);
+                        console.log("Succesful signUp of " +
+                            currentUser.username + " = " + auth
+                            .success);
+                        console.log("Message:" + JSON.stringify(
+                                auth.message) + "\n" +
+                            "message length: " + auth.length);
+                        /* This header needs to reworked so that it reports the length properly, otherwise AJAX acts weird
+                        res.writeHead(200, {'Content-Type': 'application/json','Content-Length':message.length}); */
+                        res.setHeader('X-ACCESS-TOKEN', signedJWT || null);
+                        auth = JSON.stringify(auth);
+                        res.writeHead(200, {
+                            'Content-Type': 'application/json',
+                            'Content-Length': auth.length
+                        });
+                        res.write(auth);
+
+                        console.log('response sent');
+                        console.log(res.headers);
+                        res.end();
                     }
-                console.log(auth.message);
+                    console.log("Auth message");
+                    console.log(auth.message);
                 });
-            });
-    } else if(path.slice(0,7) === "/public"){
-      //server static content out. Including JS and CSS files
-      //Unsure of how this will handle image files
-      var location = "app/" + path;
-      var type =  helper.filetype(path);
-      fs.readFile(location, function (err, data){
-        if (err) {
-          res.write("Error finding file");
-          res.end();
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/' + type,'Content-Length':data.length});
-          res.write(data);
-          res.end();
-        }
-      });
+        });
+    } else if (path === '/JWT') {
+        var jwt = '';
+        req.on("data", function(chunk) {
+            jwt += chunk;
+        });
+
+        req.on("end", function() {
+            console.log(jwt);
+            var verify = verifyJWT(jwt);
+            console.log("JWT");
+            console.log(verify);
+            res.write("FUCK YOU", 'utf8');
+            res.end();
+        });
+    } else if (path.slice(0, 7) === "/public") {
+        //server static content out. Including JS and CSS files
+        //Unsure of how this will handle image files
+        var location = "app/" + path;
+        var type = helper.filetype(path);
+        fs.readFile(location, function(err, data) {
+            if (err) {
+                res.write("Error finding file");
+                res.end();
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': 'text/' + type,
+                    'Content-Length': data.length
+                });
+                res.write(data);
+                res.end();
+            }
+        });
 
 
 
-    } else  {
-      //Base case, I don't have what you're looking
-      res.writeHead(404);
-      console.log("route fail");
-      console.log(req.headers);
-      console.log(req.body);
-      res.end();
+    } else {
+        //Base case, I don't have what you're looking
+        res.writeHead(404);
+        console.log("route fail");
+        console.log(req.headers);
+        console.log(req.body);
+        res.end();
     }
 
 }
 // A group of helper functions that either make the code in router simpler or more readable
 var helper = (function() {
-   return {
-     filetype: function (paths) {
-       return paths.split(".")[1];
-     }
-   };
+    return {
+        filetype: function(paths) {
+            return paths.split(".")[1];
+        }
+    };
 })();
 
 
-module.exports= route;
+module.exports = route;
