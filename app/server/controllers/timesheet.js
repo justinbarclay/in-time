a9adf831b169106df353eca85cccd911d96eb2e1
Justin Barclay <justincbarclay@gmail.com>
2015-08-11 09:09:12 -0600

    Cleaning up code and adding comments.

--- app/server/controllers/timesheet.js
+++ app/server/controllers/timesheet.js
@@ -12,11 +12,12 @@ var uuid = require('uuid').v4;
 //without this being set pg.defaults to 30 clients running and a timeout time for
 //each client of 30s (30000)
 pg.defaults.poolIdleTimeout = 10000;
-/** Timesheet Controller
- * This will handle creating and retrieving timesheets from pg
- *
- */
-
+////////////////////////////////////////////////////////////////////////////////
+//
+//Timesheet Controller
+//
+////////////////////////////////////////////////////////////////////////////////
+// Handles interaction with Timesheets and Timesheet_Meta tables in the database
 /* Data Structure
 *Table: Timesheet
 {
@@ -39,7 +40,13 @@ pg.defaults.poolIdleTimeout = 10000;
 //May want to look into implementing prepared queires
 // as seen at https://github.com/brianc/node-postgres/wiki/Prepared-Statements
 
+
+
 let connect = function(data) {
+    //Currently unaware of a goodway to pass around the query data, known as data
+    //and the client and done objects. I have decidced to attach them to the query
+    //itself so that at anypoint in the chain I can call client.query or client.end
+    //as well as done()
     return new Promise(function(resolve, reject) {
         pg.connect(conString, function(err, client, done) {
             if (err) {
@@ -68,6 +75,9 @@ function finish(data) {
 //Promises
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+//I may want to switch, or add streams to this data flow as well. Stream would be
+//really useful for the queries that have a for each loop
 
 ////////////////////////////////////////////////////////////////////////////////
 //Insert Queries
@@ -109,7 +119,7 @@ function addEntries(timesheet) {
             timesheet.client.query(queryString, entry, function(
                 err, result) {
                 total += 1;
-                if (err ) {
+                if (err) {
                     console.log("error line 93");
                     throw new Error(err);
                 } else {
@@ -125,6 +135,11 @@ function addEntries(timesheet) {
 ////////////////////////////////////////////////////////////////////////////////
 //Read Data Queries
 ////////////////////////////////////////////////////////////////////////////////
+//Evovling how I think about inputs, moving form timesheets to data
+//I am not qure if I like having to access the data I am querying about through
+//a property in the function. However, I am not sure of a better way to do this
+//while still being able to access client and done properties throughout the
+//querying process.
 function getTimesheetIDs(data) {
     //grabs a list of user timesheets and passes it on in a callback
     let userID = data.userID;
@@ -165,10 +180,10 @@ function getEntries(data) {
                 if (err) {
                     console.error(err);
                     throw new Error(err, 146);
-                } else if (result.rows.length > 0){
+                } else if (result.rows.length > 0) {
                     data.entries.push(result.rows);
                     if (total === timesheetIDs.length) {
-                        console.log("getEntries 153");
+                        console.log("getEntries 181");
                         resolve(data);
                     }
                 }
@@ -180,72 +195,81 @@ function getEntries(data) {
 ////////////////////////////////////////////////////////////////////////////////
 //Return Data Queries
 ////////////////////////////////////////////////////////////////////////////////
-function buildTimesheets(data){
+
+// I am unsure if this is the proper way to do this.
+// It may be better to have a singular query with a join in it so that
+function buildTimesheets(data) {
     let meta_info = data.meta;
     let entries = data.entries;
 
-    let timesheets = meta_info.map(function(meta){
+    let timesheets = meta_info.map(function(meta) {
         let timesheet = {};
         timesheet = meta.timesheetID;
         timesheet.entries = [];
-        entries.forEach(function(entry, index){
-            if (entry.timesheetID === timesheet.timesheetID){
+        entries.forEach(function(entry, index) {
+            if (entry.timesheetID === timesheet.timesheetID) {
                 timesheet.entries.push(entry);
-                entries.slice(index,1);
+                entries.slice(index, 1);
             }
         });
     });
     console.log(timesheets);
     return timesheets;
 }
-function deleteTimesheetEntries(data){
-    return new Promise(function(resolve, reject){
-                data.forEach(function(index) {
-                    data.client.query(
-                        "DELETE FROM Timesheets WHERE timesheet_foreignkey=$1", [
-                            index
-                        ],
-                        function(err, result) {
-                            if (err) {
-                                console.error(
-                                    'error deleting query into timesheet',
-                                    err);
-                                    throw new Error(err, 209);
-                            } else {
-                                console.log(
-                                    "Timesheet succesfully deleted  "
-                                );
-                                resolve(result);
-                            }
-                        });
+////////////////////////////////////////////////////////////////////////////////
+//Delete Data
+////////////////////////////////////////////////////////////////////////////////
+function deleteTimesheetEntries(data) {
+    return new Promise(function(resolve, reject) {
+        data.delete.forEach(function(index) {
+            data.client.query(
+                "DELETE FROM Timesheets WHERE timesheet_foreignkey=$1", [
+                    index
+                ],
+                function(err, result) {
+                    if (err) {
+                        console.error(
+                            'error deleting query into timesheet',
+                            err);
+                        throw new Error(err, 209);
+                    } else {
+                        console.log(
+                            "Timesheet succesfully deleted  "
+                        );
+                        resolve(data);
+                    }
                 });
-            });
-        }
+        });
+    });
+}
 
-        function deleteTimesheetMeta(data){
-            return new Promise(function(resolve, reject){
-                        data.forEach(function(index) {
-                            data.client.query(
-                                "DELETE FROM Timesheets_Meta WHERE timesheet_id=$1", [
-                                    index
-                                ],
-                                function(err, result) {
-                                    if (err) {
-                                        console.error(
-                                            'error deleting query into timesheet',
-                                            err);
-                                            throw new Error(err, 209);
-                                    } else {
-                                        console.log(
-                                            "Timesheet succesfully deleted  "
-                                        );
-                                        resolve(result);
-                                    }
-                                });
-                        });
-                    });
-                }
+function deleteTimesheetMeta(data) {
+    return new Promise(function(resolve, reject) {
+        data.delete.forEach(function(index) {
+            data.client.query(
+                "DELETE FROM Timesheets_Meta WHERE timesheet_id=$1", [
+                    index
+                ],
+                function(err, result) {
+                    if (err) {
+                        console.error(
+                            'error deleting query into timesheet',
+                            err);
+                        throw new Error(err, 209);
+                    } else {
+                        console.log(
+                            "Timesheet succesfully deleted  "
+                        );
+                        resolve(data);
+                    }
+                });
+        });
+    });
+}
 
+////////////////////////////////////////////////////////////////////////////////
+//Composed Queries
+////////////////////////////////////////////////////////////////////////////////
 function createTimesheet(data) {
     connect(data)
         .then(createMetaData)
@@ -271,11 +295,22 @@ function getTimesheets(request, res, callback) {
 
 }
 
+function deleteTimesheets(request, res, callback) {
+    connect(request)
+        .then(deleteTimesheetEntries)
+        .then(deleteTimesheetMeta)
+        .catch(error)
+        .then(finish)
+        .then(callback);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 //
-// /* TEST IMPLEMENTATION */
+//TESTS
+//
+////////////////////////////////////////////////////////////////////////////////
 // //Running some tests
-// //these tests should be move to mocha as soon as possible
-// function createTimesheetData(rowsInTimesheets){
+// //these tests should be moved to mocha as soon as possible
 function generateMetaData() {
     return {
         "timesheetID": uuid(),
@@ -308,9 +343,6 @@ function generateTimesheet(numberOfEntries) {
     return timesheet;
 }
 
-// getUserTimesheetIDs(1, function(err, bool, result){
-//     console.log(result);
-// });
 // let time = generateTimesheet();
 // createTimesheet(time);
 // let timesheet = function() {
@@ -327,9 +359,11 @@ function generateTimesheet(numberOfEntries) {
 //
 // getTimesheets({
 //     "userID": 1
-// }, null, function(data){
+// }, null, function(data) {
 //     console.log(data);
 // });
+
+deleteTimesheets({"delete": ['c54f5c44-247b-452e-8c0c-d5ef3d3ed356']}, null, function(data){console.log(data)});
 // let myTimesheet = timesheet();
 // //     myTimesheet.setTimethingy(10);
 //     console.log(myTimesheet.getTimethingy());
@@ -343,26 +377,3 @@ function generateTimesheet(numberOfEntries) {
 // console.log(generateEntry(1));
 // console.log("Generate Timesheet");
 // console.log(generateTimesheet());
-
-//
-// var baz = setTimeout(getTimesheet(function(err, bool, result){
-//     console.log("GET");
-//     if (err) {
-//         console.error("Error: ", err);
-//     } else {
-//         console.log("Returned bool: ", bool);
-//         console.log(result);
-//     }
-// }), 5000);
-//
-// baz.
-//
-// deleteArrays = Array.apply(0, Array(50)).map(function (x, y) { return y + 1; });
-// //deleteArrays = 9;
-//
-// //console.log("Is Array? " + (!Array.isArray(deleteArrays) && typeof deleteArrays !== "number"));
-// deleteTimesheet(deleteArrays, function(err, bool, result){
-//     console.error("Error delete timesheet", err);
-//     console.log("Delete operation succeeded? " + bool);
-//     console.log(result);
-// })
