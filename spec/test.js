2a7b8ee15d10ec1267060d1e28a594d993422475
Justin Barclay <justincbarclay@gmail.com>
2015-08-20 01:13:34 -0600

    Moved tests out of timesheet.js

--- spec/test.js
+++ spec/test.js
@@ -40,3 +40,79 @@ describe('Testing the user api', function (){
     done();
   }
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//TIMESHEET TESTS
+//
+////////////////////////////////////////////////////////////////////////////////
+// //Running some tests
+// //these tests should be moved to mocha as soon as possible
+function generateMetaData(id) {
+    return {
+        "timesheetID": uuid(),
+        "userID": 1,
+        "start_date": "2015/01/01",
+        "end_date": "2015/01/15",
+        "engagement": Math.floor(Math.random() * 10000)
+    };
+}
+
+function generateEntry(id) {
+    return {
+        "timesheet_foreignkey": id,
+        "service_duration": (Math.floor(Math.random() * 12)),
+        "service_description": "description of service",
+        "service_date": "05/07/2015"
+    };
+
+}
+
+function generateTimesheet(numberOfEntries) {
+    var timesheet = generateMetaData();
+    var entries = [];
+    var rows = numberOfEntries ? numberOfEntries : Math.floor(Math.random() * 7 +
+        3);
+    for (let i = 0; rows > i; i++) {
+        entries.push(generateEntry(timesheet.timesheetID));
+    }
+    timesheet.entries = entries;
+    return timesheet;
+}
+
+// let time = generateTimesheet();
+// createTimesheet(time);
+// let timesheet = function() {
+//     let timethingy = {};
+//     return {
+//         set: function(obj) {
+//             timethingy = obj;
+//         },
+//         get: function() {
+//             return timethingy;
+//         }
+//     };
+// };
+//
+// getTimesheets({
+//     "userID": 1
+// });
+
+
+
+// let queryString = "SELECT timesheets_meta.timesheet_id, timesheets_meta.user_foreignkey, timesheets_meta.start_date, timesheets_meta.end_date, timesheets_meta.engagement, timesheets.service_description, timesheets.service_duration, timesheets.service_date FROM employees.public.timesheets_meta, employees.public.timesheets WHERE timesheets_meta.timesheet_id = timesheets.timesheet_foreignkey AND timesheets_meta.timesheet_id = $1";
+// deleteTimesheets({"delete": ['c54f5c44-247b-452e-8c0c-d5ef3d3ed356']}, null, function(data){console.log(data)});
+// let myTimesheet = timesheet();
+// //     myTimesheet.setTimethingy(10);
+//     console.log(myTimesheet.getTimethingy());
+// connect({"userID": 1}).then(getTimesheetIDs).then(function(data){
+//     myTimesheet.set(data)
+//     return data;
+// }).catch(error).then(finish);
+// console.log("generateMetaData");
+// console.log(generateMetaData());
+// console.log("generateEntry");
+// console.log(generateEntry(1));
+// console.log("Generate Timesheet");
+// console.log(generateTimesheet());
