a5ae042a09a62cec8aecfe02cc4c52c1cd395067
Justin Barclay <justincbarclay@gmail.com>
2016-01-07 13:37:27 -0700

    Update database creaiton scripts

--- scripts/upsertFunctions.js
+++ scripts/upsertFunctions.js
@@ -55,18 +55,29 @@ function createFunctions() {
     $$
     LANGUAGE plpgsql;`;
 
+    var upsertMetaLocked =
+    `CREATE OR REPLACE FUNCTION upsert_meta(key TEXT, user_id INTEGER, sDate DATE, eDate DATE, engagement_num INTEGER, remove BOOLEAN ) RETURNS VOID AS
+    $$
+    BEGIN
+        LOCK TABLE timesheets_meta IN SHARE ROW EXCLUSIVE MODE;
+        WITH upsert_meta AS (UPDATE timesheets_meta SET timesheet_id = key, user_foreignkey = user_id, start_date = sDate, end_date = eDate, engagement = engagement_num, delete = remove WHERE timesheet_id = key RETURNING *)
+        INSERT INTO timesheets_meta(timesheet_id, user_foreignkey, start_date, end_date, engagement, delete) SELECT key, user_id, sDate, eDate, engagement_num, remove WHERE NOT EXISTS (SELECT * FROM upsert_meta);
+    END;
+    $$
+    LANGUAGE plpgsql;`;
+
     var upsertLockedTimesheet =
     `CREATE or REPLACE FUNCTION upsert_timesheet(key TEXT, id TEXT, description TEXT, duration REAL, sDate DATE, remove BOOLEAN ) RETURNS VOID AS
     $$
     BEGIN
         LOCK TABLE timesheets IN SHARE ROW EXCLUSIVE MODE;
-	    WITH upsert AS (UPDATE timesheets SET service_description = description, service_date = sDate, service_duration = duration, delete = remove WHERE timesheet_foreignkey = key and row_id = id RETURNING *)
-	    INSERT INTO timesheets(timesheet_foreignkey, row_id, service_description, service_date, service_duration, delete) SELECT key, id, description, sDate,  duration, remove WHERE NOT EXISTS (SELECT * FROM upsert);
+	    WITH upsert_entries AS (UPDATE timesheets SET service_description = description, service_date = sDate, service_duration = duration, delete = remove WHERE timesheet_foreignkey = key and row_id = id RETURNING *)
+        INSERT INTO timesheets(timesheet_foreignkey, row_id, service_description, service_date, service_duration, delete) SELECT key, id, description, sDate,  duration, remove WHERE NOT EXISTS (SELECT * FROM upsert_entries);
     END;
     $$
     LANGUAGE plpgsql;`;
     try {
-        client.query(upsertMeta);
+        client.query(upsertMetaLocked);
         client.query(upsertLockedTimesheet);
     } catch (error) {
         console.error("this is an error", error);
