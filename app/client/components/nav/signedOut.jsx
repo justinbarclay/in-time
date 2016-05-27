21d95847b2bc4e1fc3fa81a57d696d05099eb9c1
Justin Barclay <justincbarclay@gmail.com>
2016-05-26 23:06:13 -0600

    More demo things

--- app/client/components/nav/signedOut.jsx
+++ app/client/components/nav/signedOut.jsx
@@ -2,15 +2,22 @@ var React = require("react");
 var Link = require("react-router").Link;
 var Router = require("react-router").Router;
 var Route = require("react-router").Route;
+var demoActions = require("../../actions/demoActions");
 
 var NavSignedOut = React.createClass({
     displayName: "signed out",
     propTypes: {},
+    setUpDemo: function(){
+        demoActions.loadDemo();
+    },
     render: function() {
         return (
             <div className="navigation">
                 <Link to="/register" className="nav" >
                     <label>Register</label>
+                </Link>
+                <Link to="/demo" className="nav" >
+                    <label>Demo</label>
                 </Link>
                 <Link to="/signin" className="nav">
                     <label>Sign In</label>
