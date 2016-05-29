0abee78eb89684c0b9fa5264c7c9df1db69937af
Justin Barclay <justincbarclay@gmail.com>
2016-05-28 22:45:47 -0600

    Make scripts to insert demo data into database

--- scripts/createUserDB.js
+++ scripts/createUserDB.js
@@ -6,12 +6,12 @@ var conString = config.postgres;
 function createUserDB() {
     var client = new pg.Client(conString);
     client.connect();
-    client.query("DROP TABLE IF EXISTS UserLogin");
+    client.query("DROP TABLE IF EXISTS Users");
     // Creat table and insert 2 records into it
 
     try {
         client.query(
-            "CREATE TABLE IF NOT EXISTS Users ( user_id serial PRIMARY KEY, email varchar(32) UNIQUE, password varchar(64), role TEXT, invite_code varchar(36), invited_on timestamptz, supervisor varchar(32), org_foreignkey INTEGER, last_accessed timestamptz )"
+            "CREATE TABLE IF NOT EXISTS Users( user_id serial PRIMARY KEY, email varchar(32) UNIQUE, password varchar(64), role TEXT, invite_code varchar(36), invited_on timestamptz, supervisor INTEGER, org_foreignkey INTEGER, last_accessed timestamptz )"
         );
     } catch (error) {
         console.log(error);
---
+++ scripts/setUpDemo.js
@@ -0,0 +1,209 @@
+let owner = require('../app/server/controllers/owner');
+let data = require('./demodata');
+
+let config = require("../config.js");
+let pg = require('pg');
+let Client = pg.Client;
+let conString = config.postgres;
+
+let demoOwner = {
+    "user":{
+        "email": data.owner.email,
+        "password":1
+    }, "org": {
+        "orgname": data.owner.organization,
+        "domain": null
+    }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Database Boilerplate
+//
+////////////////////////////////////////////////////////////////////////////////
+let connect = function(data) {
+    //Currently unaware of a goodway to pass around the query data, known as data
+    //and the client and done objects. I have decidced to attach them to the query
+    //itself so that at anypoint in the chain I can call client.query or client.end
+    //as well as done()
+    return new Promise(function(resolve, reject) {
+        pg.connect(conString, function(err, client, done) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(data);
+                resolve({
+                    setup: data,
+                    done: done,
+                    client: client
+                });
+            }
+        });
+    });
+};
+function rollback(data) {
+    //if there was a problem rolling back the query
+    //something is seriously messed up.  Return the error
+    //to the done function to close & remove this client from
+    //the pool.  If you leave a client in the pool with an unaborted
+    //transaction weird, hard to diagnose problems might happen.
+    console.log(data);
+    return new Promise(function(resolve, reject) {
+        data.client.query('ROLLBACK', function(err) {
+            data.done();
+            if (err) {
+                throw err;
+            } else {
+                resolve({
+                    setup: data.setup,
+                    client: data.client,
+                    done: data.done
+                });
+            }
+        });
+    });
+}
+
+function begin(data) {
+    //as long as we do not call the `done` callback we can do
+    //whatever we want...the client is ours until we call `done`
+    //on the flip side, if you do call `done` before either COMMIT or ROLLBACK
+    //what you are doing is returning a client back to the pool while it
+    //is in the middle of a transaction.
+    //Returning a client while its in the middle of a transaction
+    //will lead to weird & hard to diagnose errors.
+    return new Promise(function(resolve, reject) {
+        data.client.query('BEGIN', function(err) {
+            if (err) {
+                throw err;
+            } else {
+                resolve({
+                    setup: data.setup,
+                    client: data.client,
+                    done: data.done
+                });
+            }
+        });
+    });
+}
+function finish(data) {
+    return new Promise(function(resolve, reject) {
+        data.client.query('COMMIT', data.done);
+        data.done();
+        data.message = "Timesheet updated";
+        resolve(data);
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
+//
+// Promises
+//
+////////////////////////////////////////////////////////////////////////////////
+makeDemo = function(data){
+    return new Promise(function(resolve, reject){
+        let demoOwner = {
+            "user":{
+                "email": data.owner.email,
+                "password":"1"
+            }, "org": {
+                "orgname": data.owner.organization,
+                "domain": null
+            }
+        };
+        try{
+            owner.addOrganization(demoOwner, function(owner){
+                console.log(owner);
+                data.owner.ownerID = owner.userID;
+                data.orgID = owner.orgID;
+                resolve(data);
+            });
+        } catch (err){
+            reject(err);
+        }
+    });
+};
+
+function addSupervisors(data){
+    return new Promise(function(resolve, reject){
+        let supervisors = data.setup.supervisors;
+        Promise.all(supervisors.map(function(supervisor) {
+            supervisor.orgID = data.setup.orgID;
+            return addEmployee(supervisor, data.client);
+        }))
+        .then(function(result) {
+            data.setup.supervisors = result;
+            console.log(result);
+            resolve({
+                setup: data.setup,
+                client: data.client,
+                done: data.done
+            });
+        });
+    });
+}
+
+function addStaff(data){
+    console.log("Starting staff");
+    return new Promise(function(resolve, reject){
+        let staff = data.setup.staff;
+        Promise.all(staff.map(function(user) {
+                user.orgID = data.setup.orgID;
+                return addEmployee(user, data.client);
+            }))
+        .then(function(result) {
+            console.log("add staff");
+            resolve({
+                data: data.setup,
+                client: data.client,
+                done: data.done
+            });
+        })
+        .catch(reject);
+    });
+}
+function addSupervisorID(data){
+    console.log(data);
+    let employees = data.setup.staff;
+    let supervisors = data.setup.supervisors;
+    employees.map(function(employee){
+        for(i=0; i< supervisors.length; i++){
+            if(employee.supervisor === supervisors[i].email){
+                employee.supervisorID = supervisors[i].id;
+            }
+        }
+        return employee;
+    });
+    return data;
+}
+function addEmployee(data, client) {
+    let queryString = "INSERT into Users(email, password, org_foreignkey, role, supervisor) VALUES ($1, $2, $3, $4, $5) RETURNING user_id";
+    //Asynchronously insert data into the database
+    var employee = [
+        data.email, null, data.orgId, data.role, data.supervisorID || null
+    ];
+    return new Promise(function(resolve, reject) {
+        client.query(queryString, employee, function(err, result) {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(result);
+                data.id = result.rows[0].user_id;
+                resolve(data);
+            }
+        });
+    });
+}
+
+makeDemo(data)
+.then(connect)
+.then(begin)
+.then(addSupervisors)
+.then(addSupervisorID)
+.then(addStaff)
+.catch(rollback)
+.then(finish)
+.then(function(){
+    console.log("done");
+});
